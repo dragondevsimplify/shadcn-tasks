0e9d1db2a9860b7b0723c0ce1900966ebd184c20
dragondevsimplify <dragondevsimplify@gmail.com>
2024-12-17 15:26:32 +0700

    Install react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import { Tasks } from "@/components/tasks/tasks.tsx";
+import { Routes, Route } from "react-router";
+import { TasksEditPage } from "@/components/tasks/tasks-edit-page.tsx";
 
 function App() {
   return (
     <main className="p-4">
-      <Tasks />
+      <Routes>
+        <Route path="/" element={<Tasks />} />
+        <Route path="/tasks/edit/:id" element={<TasksEditPage />} />
+      </Routes>
     </main>
   )
 }
---
+++ src/components/tasks/tasks-edit-page.tsx
@@ -0,0 +1,5 @@
+export function TasksEditPage() {
+  return (
+    <h1>Hello</h1>
+  )
+}
